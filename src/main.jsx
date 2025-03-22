7a69e2c1edcd1e663d51f01ae946c02dc19fa4a5
Ayub40 <ayubk4028@gmail.com>
2025-03-23 00:34:28 +0600

    added glassmorphism effect in navbar

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { router } from './Routes/Routes';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='max-w-screen-[1700px] mx-auto'>
+    {/* <div className='max-w-screen-xl mx-auto'> */}
       <RouterProvider router={router} />
     </div>
   </StrictMode>,
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -10,43 +10,45 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar fixed z-10 bg-base-100 lg:max-w-screen-[1700px] m-auto ">
-            {/* <div className='absolute inset-0 blur-xl ' style={{ background: "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)" }}></div> */}
-            <div className=""></div>
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
+        <div className=" bg-opacity-90 fixed z-10 w-full h-20 backdrop-blur-lg bg-white/30 border-b border-white/20 shadow-sm">
+            <div className="navbar max-w-screen-xl mx-auto h-full">
+                {/* <div className='absolute inset-0 blur-xl ' style={{ background: "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)" }}></div> */}
+                <div className=""></div>
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {navOptions}
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    {/* <a className="btn btn-ghost text-xl">EduNova</a> */}
+                    <button><img src="https://i.ibb.co.com/q3zhWkty/EduNova.png" width={150} className="mx-auto" /></button>
+                    {/* <img src="https://i.ibb.co.com/hRY2Q8H2/Edu-Nova-2.png" width={150} className="mx-auto" /> */}
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
                         {navOptions}
                     </ul>
                 </div>
-                {/* <a className="btn btn-ghost text-xl">EduNova</a> */}
-                <button><img src="https://i.ibb.co.com/q3zhWkty/EduNova.png" width={150}  className="mx-auto" /></button>
-                {/* <img src="https://i.ibb.co.com/hRY2Q8H2/Edu-Nova-2.png" width={150} className="mx-auto" /> */}
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navOptions}
-                </ul>
-            </div>
-            <div className="navbar-end gap-5">
-                <Link to='/signIn'><button className="btn  md:text-xl">Sign In</button></Link>
-                <Link to='/signUp'><button className="btn  md:text-xl">Sign Up</button></Link>
+                <div className="navbar-end gap-5">
+                    <Link to='/signIn'><button className="btn  md:text-xl">Sign In</button></Link>
+                    <Link to='/signUp'><button className="btn  md:text-xl">Sign Up</button></Link>
+                </div>
             </div>
         </div>
     );
