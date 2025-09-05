3019a97445a3714f8d835693deb1946c7b845710
Ayub40 <ayubk4028@gmail.com>
2025-09-06 01:56:38 +0600

    update design

--- src/Pages/Home/AboutUs.jsx
+++ src/Pages/Home/AboutUs.jsx
@@ -4,16 +4,16 @@ import AboutUsTab from "../../components/AboutUs/AboutUsTab";
 
 const AboutUs = () => {
     return (
-        <div className="flex mb-24 max-w-7xl mx-auto gap-16">
+        <div className="flex mb-24 max-w-7xl mx-auto ">
             <div className="flex mt-2 w-[620px] h-[550px] relative">
                 <div className="h-[371px] w-[294px]">
                     {/* <img className="mt-3 ml-1 outline outline-offset-4 outline-2 outline-[#DDE1E7] rounded-tr-[60px] rounded-bl-[60px]"
                         src="https://html.rrdevs.net/edcare/assets/img/images/about-img-1.jpg" alt="" /> */}
                     <img className="mt-3 ml-1 outline outline-offset-4 outline-2 outline-[#DDE1E7] rounded-tr-[60px] rounded-bl-[60px]"
                         src="	https://eduna-template.vercel.app/eduna/assets/images/team/team-1/3.png" alt="" />
-                    <div className="flex gap-3 h-[110px] w-[290px] border border-[#07A698] bg-[#24A7BC] rounded-tr-[60px] rounded-bl-[60px] mt-8 place-content-center place-items-center">
+                    <div className="flex gap-3 h-[110px] w-[290px] border border-[#07A698] bg-[#24A7BC] rounded-tr-[60px] rounded-bl-[60px] mt-12 place-content-center place-items-center">
                         {/* <div className="flex gap-3 h-[110px] w-[290px] border border-[#07A698] bg-[#07A698] rounded-tr-[60px] rounded-bl-[60px] mt-8 place-content-center place-items-center"> */}
-                        <div className="h-12 w-12 bg-white rounded-full p-3 place-content-center">
+                        <div className="h-12 w-12 bg-white rounded-full p-3 place-content-center ">
                             <MdWifiCalling3 className="h-[20px] w-[20px] text-[#07A698]" />
                         </div>
                         <div className="">
